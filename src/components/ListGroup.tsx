d2f8374e8c4d0fdbed12ac343ff6881f42007107
Shamonti <67649277+Shamonti@users.noreply.github.com>
2024-04-04 15:47:18 +0600

    Added condition for empty list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,14 @@
 // import { Fragment } from 'react/jsx-runtime';
 
 function ListGroup() {
-  const items = ['Dinajpur', 'Rangpur', 'Thakurgaon', 'Paharpur', 'Saidpur'];
+  let items = ['Dinajpur', 'Rangpur', 'Thakurgaon', 'Paharpur', 'Saidpur'];
+  // items = [];
+
   return (
     <>
       <h1>List</h1>
       <ul className='list-group'>
+        {items.length === 0 && <p>No item found.</p>}
         {items.map(item => (
           <li key={item}>{item}</li>
         ))}
