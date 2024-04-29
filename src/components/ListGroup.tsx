621ab941bd09f1286a98d1117478b6aedb2e3329
Shamonti <67649277+Shamonti@users.noreply.github.com>
2024-04-29 18:30:34 +0600

    added conditional statement

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -7,10 +7,16 @@ function ListGroup() {
   return (
     <>
       <h1>List</h1>
+      {items.length == 0 && <p>No items found.</p>}
       <ul className='list-group'>
-        {items.length === 0 && <p>No item found.</p>}
         {items.map(item => (
-          <li key={item}>{item}</li>
+          <li
+            key={item}
+            className='list-group-item'
+            onClick={() => console.log({ item })}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
