84ea1c1dc4ab5b135c7a602b1bb2c209c56125f6
Shamonti <shamonti.haque98@gmail.com>
2024-06-11 14:29:28 +0600

    Passed function via props

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import ListGroup from './components/ListGroup';
 function App() {
   let items = ['Dinajpur', 'Rangpur', 'Thakurgaon', 'Paharpur', 'Saidpur'];
 
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading='Cities' />
+      <ListGroup
+        items={items}
+        heading='Cities'
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
