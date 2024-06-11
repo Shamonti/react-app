34a0467a243650f37d85e5ead1b262003e14adf5
Shamonti <shamonti.haque98@gmail.com>
2024-06-11 13:13:03 +0600

    Added useState hook to ListGroup

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,34 @@
 // import { Fragment } from 'react/jsx-runtime';
+// import { MouseEvent } from 'react';
+
+import { useState } from 'react';
 
 function ListGroup() {
   let items = ['Dinajpur', 'Rangpur', 'Thakurgaon', 'Paharpur', 'Saidpur'];
-  // items = [];
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  // Event Handler
+  // const handleClick = (event: MouseEvent) => {
+  //   console.log(event);
+  // };
 
   return (
     <>
       <h1>List</h1>
       {items.length == 0 && <p>No items found.</p>}
       <ul className='list-group'>
-        {items.map(item => (
+        {items.map((item, index) => (
           <li
-            key={item}
-            className='list-group-item'
-            onClick={() => console.log({ item })}
+            className={
+              selectedIndex === index
+                ? 'list-group-item active'
+                : 'list-group-item'
+            }
+            key={index}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
           >
             {item}
           </li>
