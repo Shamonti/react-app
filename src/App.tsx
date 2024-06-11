89f68af14c7276c6c1543c8cf02d87e9c8ab81ca
Shamonti <shamonti.haque98@gmail.com>
2024-06-11 14:38:27 +0600

    Passed children via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import ListGroup from './components/ListGroup';
+import Alert from './components/Alert';
+// import ListGroup from './components/ListGroup';
 
 function App() {
-  let items = ['Dinajpur', 'Rangpur', 'Thakurgaon', 'Paharpur', 'Saidpur'];
+  // let items = ['Dinajpur', 'Rangpur', 'Thakurgaon', 'Paharpur', 'Saidpur'];
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
 
   return (
     <div>
-      <ListGroup
+      {/* <ListGroup
         items={items}
         heading='Cities'
         onSelectItem={handleSelectItem}
-      />
+      /> */}
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
