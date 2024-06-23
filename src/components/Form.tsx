3dc5cb2264d7ede2d8d546c8ef5dce646b25b32b
Shamonti <shamonti.haque98@gmail.com>
2024-06-23 13:09:48 +0600

    Added button disabling function

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -25,7 +25,7 @@ const Form = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => {
@@ -78,7 +78,7 @@ const Form = () => {
         />
         {errors.age && <p className='text-danger'>{errors.age.message}</p>}
       </div>
-      <button className='btn btn-primary' type='submit'>
+      <button disabled={!isValid} className='btn btn-primary' type='submit'>
         Submit
       </button>
     </form>
